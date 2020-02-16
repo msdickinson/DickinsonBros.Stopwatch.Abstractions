376f42c62e8701a9143f8cd35eaae248a24d312a
Mark Dickinson <marksamdickinson@gmail.com>
2020-02-16 13:49:52 -0700

    Add project files.

---
+++ DickinsonBros.Stopwatch.Abstractions/IStopwatchService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DickinsonBros.Stopwatch.Abstractions
+{
+    public interface IStopwatchService
+    {
+        public void Start();
+        public void Stop();
+        public void Reset();
+        public TimeSpan Elapsed { get; }
+        public long ElapsedMilliseconds { get; }
+    }
+}
